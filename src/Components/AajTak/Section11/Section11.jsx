1851376339c3373d12e45d362d4979d246e0f601
umesh sahu <umeshsahu32@gmail.com>
2024-07-30 17:14:49 +0530

    user frontend

--- src/Components/AajTak/Section11/Section11.jsx
+++ src/Components/AajTak/Section11/Section11.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Mousewheel, Autoplay } from "swiper/modules";
 import "swiper/css";
@@ -7,42 +7,57 @@ import styles from "./Section11.module.css";
 
 const Section11 = () => {
   const stats = [
-    { number: "500+", label: "Clients" },
-    { number: "11+", label: "Countries" },
-    { number: "1200cr", label: "Revenue" },
+    { number: "500+", label: "Clients Served" },
+    { number: "1 M+", label: "Leads Generated" },
+    { number: "200+", label: "Sales Expert" },
     { number: "100+", label: "Awards" },
   ];
 
   return (
-    <section className={styles.section}>
-      <div className={styles.content}>
-        <h2 className={styles.heading}>Big Dreams, Bigger Numbers</h2>
-        <p className={styles.paragraph}>
-          We are moving ahead with relentless energy to achieve amazing results
-          that speak volumes. We don't intend to slow down either!
-        </p>
-      </div>
-      <div className={styles.carouselContainer}>
-        <Swiper
-          direction="vertical"
-          mousewheel={true}
-          loop={true}
-          autoplay={{
-            delay: 2000,
-            disableOnInteraction: false,
-          }}
-          modules={[Mousewheel, Autoplay]}
-          className={styles.swiper}
-        >
-          {stats.map((stat, index) => (
-            <SwiperSlide key={index} className={styles.slide}>
-              <div className={styles.statNumber}>{stat.number}</div>
-              <div className={styles.statLabel}>{stat.label}</div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    </section>
+    <Fragment>
+      <section className={styles.section}>
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <h2 className={styles.heading}>
+              Big Dreams,
+              <br /> Bigger Numbers
+            </h2>
+            <p className={styles.paragraph}>
+              We are moving ahead with relentless energy to achieve amazing
+              results that speak volumes. We don't intend to slow down either!
+            </p>
+          </div>
+          <div className={styles.carouselContainer}>
+            <Swiper
+              direction="vertical"
+              mousewheel={true}
+              loop={true}
+              autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+              }}
+              modules={[Mousewheel, Autoplay]}
+              className={styles.swiper}
+            >
+              {stats.map((stat, index) => (
+                <SwiperSlide key={index} className={styles.slide}>
+                  <div className={styles.statNumber}>{stat.number}</div>
+                  <div className={styles.statLabel}>{stat.label}</div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+          <div className={styles.gridContainer}>
+            {stats.map((stat, index) => (
+              <div key={index} className={styles.gridItem}>
+                <div className={styles.number}>{stat.number}</div>
+                <div className={styles.label}>{stat.label}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </Fragment>
   );
 };
 
